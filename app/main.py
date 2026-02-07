35f298b075021266533e306dcf2dec7cb9dc6b0f
Vavazzzz <nicola.vavassori@hotmail.it>
2026-02-07 02:22:08 +0100

    added uvicorn run in main

--- app/main.py
+++ app/main.py
@@ -5,3 +5,9 @@ app = FastAPI(title="League Standings Service")
 
 app.include_router(health.router)
 app.include_router(data.router)
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
